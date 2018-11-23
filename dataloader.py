1057c8ce976dfa0e784540b0bf6ec69ce341e996
Lizhe Chen <lizhe.chen@mail.utoronto.ca>
2018-11-23 03:25:13 -0500

    finish model building

--- dataloader.py
+++ dataloader.py
@@ -9,56 +9,94 @@ from torch.utils.data import Dataset, DataLoader
 from torchvision import transforms, utils
 import pickle as pkl
 
+from keras.preprocessing.image import ImageDataGenerator
+
 # Ignore warnings
 import warnings
 warnings.filterwarnings("ignore")
 
 # plt.ion()   # interactive mode
 # plt.interactive(False)
+#
+# class HanziTrainingDataset(Dataset):
+#
+#     def __init__(self, rootDir, charDictDir, transform=None):
+#         with open(charDictDir, 'rb') as handler:
+#             self.charDict = pkl.load(handler)
+#         self.rootDir = rootDir
+#         self.sampleNumList = np.zeros(len(self.charDict))
+#         self.sampleFilesDict = {}
+#         self.transform = transform
+#         for c in range(len(self.charDict)):
+#             samplesPath = os.path.join(self.rootDir, '{:05}'.format(c))
+#             self.sampleFilesDict[c] = []
+#
+#             for files in os.listdir(samplesPath):
+#                 if files.endswith(".png"):
+#                     self.sampleFilesDict[c].append(files)
+#
+#             self.sampleNumList[c] = np.int(len(self.sampleFilesDict[c]))
+#         self.sampleCumList = np.int32(np.cumsum(self.sampleNumList))
+#
+#     def __len__self(self):
+#         return self.sampleCumList[-1]
+#
+#     def __getitem__(self, idx):
+#         label = (self.sampleCumList < idx).sum()
+#         labelPath = '{:05}'.format(label)
+#         if label > 0:
+#             index = idx - self.sampleCumList[label-1]
+#         else:
+#             index = idx
+#         fileIndex = self.sampleFilesDict[label][index]
+#
+#         imgName = os.path.join(self.rootDir, labelPath, fileIndex)
+#         image = io.imread(imgName)
+#         sample = {'image': image, 'label': label}
+#
+#         if self.transform:
+#             sample = self.transform(sample)
+#
+#         return sample
+#
+# trainDataset = HanziTrainingDataset(rootDir='./data/image_data/train', charDictDir='./data/char_dict')
+#
+# print(trainDataset[8995]['label'])
 
-class HanziTrainingDataset(Dataset):
+# plt.imshow(trainDataset[8995]['image'].shape)
+# plt.show()
 
-    def __init__(self, rootDir, charDictDir, transform=None):
-        with open(charDictDir, 'rb') as handler:
-            self.charDict = pkl.load(handler)
-        self.rootDir = rootDir
-        self.sampleNumList = np.zeros(len(self.charDict))
-        self.sampleFilesDict = {}
-        self.transform = transform
-        for c in range(len(self.charDict)):
-            samplesPath = os.path.join(self.rootDir, '{:05}'.format(c))
-            self.sampleFilesDict[c] = []
 
-            for files in os.listdir(samplesPath):
-                if files.endswith(".png"):
-                    self.sampleFilesDict[c].append(files)
+# with open('./data/char_dict', 'rb') as handler:
+#     charDict = pkl.load(handler)
+# classSize = len(charDict)
 
-            self.sampleNumList[c] = np.int(len(self.sampleFilesDict[c]))
-        self.sampleCumList = np.int32(np.cumsum(self.sampleNumList))
 
-    def __len__self(self):
-        return self.sampleCumList[-1]
+def validGenerator(path, batchSize, width, height):
+    valid_datagen = ImageDataGenerator(rescale=1. / 255)
 
-    def __getitem__(self, idx):
-        label = (self.sampleCumList < idx).sum()
-        labelPath = '{:05}'.format(label)
-        if label > 0:
-            index = idx - self.sampleCumList[label-1]
-        else:
-            index = idx
-        fileIndex = self.sampleFilesDict[label][index]
-        print(fileIndex)
-        imgName = os.path.join(self.rootDir, labelPath, fileIndex)
-        image = io.imread(imgName)
-        sample = {'image': image, 'label': label}
+    valid_generator = valid_datagen.flow_from_directory(
+        path,
+        target_size=(width, height),
+        batch_size=batchSize,
+        color_mode="grayscale",
+        class_mode='categorical')
 
-        if self.transform:
-            sample = self.transform(sample)
+    return valid_generator
 
-        return sample
-trainDataset = HanziTrainingDataset(rootDir='./data/image_data/train', charDictDir='./data/char_dict')
 
-print(trainDataset[8995]['label'])
+def trainGenerator(path, batchSize, width, height):
+    train_datagen = ImageDataGenerator(
+        rescale=1. / 255,
+        rotation_range=5,
+        width_shift_range=0.05,
+        height_shift_range=0.05)
 
-plt.imshow(trainDataset[8995]['image'])
-plt.show()
+    train_generator = train_datagen.flow_from_directory(
+        path,
+        target_size=(width, height),
+        batch_size=batchSize,
+        color_mode="grayscale",
+        class_mode='categorical')
+
+    return train_generator
---
+++ training.py
@@ -0,0 +1,49 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pickle as pkl
+import os
+
+import keras
+from keras.models import Model, load_model
+from keras import backend as K
+from keras.preprocessing.image import ImageDataGenerator
+from keras import optimizers
+from keras.callbacks import ModelCheckpoint
+
+from dataloader import trainGenerator, validGenerator
+from model_9_layer_GSLRE import build_model_GSLRE
+
+
+rootDir='./data/image_data'
+trainDir = os.path.join(rootDir, 'train')
+validDir = os.path.join(rootDir, 'test')
+
+with open('./data/char_dict', 'rb') as handler:
+    charDict = pkl.load(handler)
+classSize = len(charDict)
+
+batchSize = 1024
+val_batchSize = 1024
+width, height = 96, 96
+
+vg = validGenerator(validDir, val_batchSize, 96, 96)
+tg = trainGenerator(trainDir, batchSize, 96, 96)
+
+model = build_model_GSLRE(classSize)
+
+sgd = optimizers.SGD(lr=0.1, momentum=0.9, nesterov=True)
+model.compile(loss='categorical_crossentropy',
+        optimizer='SGD',
+        metrics=['accuracy'])
+
+checkpointer = keras.callbacks.ModelCheckpoint(filepath='model.weights.best.hdf5',
+                                               verbose = 0, save_best_only=True, monitor='val_acc')
+# sgd = SGD(lr=0.1, decay=0, momentum=0.9, nesterov=True)
+# K.set_value(sgd.lr, 0.5 * K.get_value(sgd.lr))
+
+model.fit_generator(tg, epochs=5, verbose=1,
+                    validation_data=vg,
+                    steps_per_epoch=len(tg),
+                    validation_steps=len(vg),
+                    callbacks=[checkpointer],
+                    use_multiprocessing=True)
